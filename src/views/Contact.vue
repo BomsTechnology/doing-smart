543c2dce063f0f9c23ca48cd6e0e54ea84f265f5
BomsTechnology <marcsigha@gmail.com>
2022-11-13 15:22:00 +0100

    fix some error

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -29,7 +29,7 @@ const links: Array<Link> = [
             class="w-full h-full"
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1614.0037904658373!2d9.74186621096476!3d4.043597282872684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2scm!4v1668340031208!5m2!1sen!2scm"
             style="border: 0"
-            allowfullscreen=""
+            allowfullscreen="true"
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
